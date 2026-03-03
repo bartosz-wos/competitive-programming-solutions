8ef162c9d8a4b0eba6acf4af7b13d0668fca4cf2
Bartosz Wos <bartosz.wos.cs@gmail.com>
2026-03-03 20:32:29 +0100

    Add solution to play onwards

---
+++ FXCUP/play-onwards-guess.cpp
@@ -0,0 +1,7 @@
+#include "guess.h"
+
+std::string GuessWord(int M, std::string YH){
+	std::string R;
+	for(int i=0;i<M;i++) R += 'a';
+	return R;
+}
---
+++ FXCUP/play-onwards-make.cpp
@@ -0,0 +1,7 @@
+#include "make.h"
+
+std::string MakeWord(int N, int M, std::vector<std::string> Dict){
+	std::string R;
+	for(int i=0;i<M;i++) R += 'a';
+	return R;
+}
