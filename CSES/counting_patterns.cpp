267301ff693ac2d2e0838d51f2bd34626cdebf3f
bartosz-wos <bartosz.wos.cs@gmail.com>
2026-04-14 21:52:26 +0200

    Add solution to counting patterns

---
+++ CSES/counting_patterns.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+ 
+ll sa[100001];
+ll pos[100001];
+ll tmp[100001];
+ll gap, n;
+string s;
+ 
+bool comp(ll x, ll y){
+	if(pos[x]!=pos[y])
+		return pos[x]<pos[y];
+	x+=gap;
+	y+=gap;
+	return (x<n and y<n)?pos[x]<pos[y]:x>y;
+}
+ 
+void suffix(){
+	for(ll i=0;i<n;++i)
+		sa[i]=i, pos[i]=s[i];
+	for(gap=1;;gap<<=1){
+		sort(sa,sa+n,comp);
+		for(ll i=0;i<n-1;++i)
+			tmp[i+1]=tmp[i]+comp(sa[i],sa[i+1]);
+		for(ll i=0;i<n;++i)
+			pos[sa[i]]=tmp[i];
+		if(tmp[n-1]==n-1)
+			break;
+	}
+}
+ 
+ll check(ll m, const string& x){
+	ll f=-1;
+	ll k=x.size();
+	ll j=sa[m];
+	if(n-j>=k)
+		f=0;
+	for(ll i=0;i<min(n-j,k);++i){
+		if(s[j+i]<x[i])
+			return -1;
+		if(s[j+i]>x[i])
+			return 1;
+	}
+	return f;
+}
+ 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin>>s;
+	n=s.length();
+	suffix();
+	ll q;
+	cin>>q;
+	while(q--){
+		string x;
+		cin>>x;
+		ll l=0, r=n-1;
+		ll f=-1;
+		while(l<=r){
+			ll mid=(l+r)>>1;
+			int v=check(mid,x);
+			if(v==0){
+				f=mid;
+				r=mid-1;
+			}
+			else if(v==1)
+				r=mid-1;
+			else
+				l=mid+1;
+		}
+		if(f==-1){
+			cout<<0<<'\n';
+			continue;
+		}
+		ll L=f;
+		ll R=n-1;
+		l=f;
+		r=n-1;
+		while(l<=r){
+			ll mid=(l+r)>>1;
+			ll v=check(mid, x);
+			if(v==0){
+				f=mid;
+				l=mid+1;
+			}
+			else if(v==-1)
+				l=mid+1;
+			else
+				r=mid-1;
+		}
+		R=f;
+		cout<<R-L+1<<'\n';
+	}
+}
