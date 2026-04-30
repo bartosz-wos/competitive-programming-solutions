d18f0bba304599891b48e1ba41bafc60da3a47f3
bartosz-wos <bartosz.wos.cs@gmail.com>
2026-04-30 21:27:16 +0200

    Add solution to mex grid queries

---
+++ CSES/mex-grid-queries.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    int y,x;
+    cin>>y>>x;
+    --y,--x;
+    cout<<(y^x)<<'\n';
+}
